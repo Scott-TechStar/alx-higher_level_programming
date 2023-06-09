fc544d052d4aeb4138a48f5843b945aa80be27aa
Scott-TechStar <johnniekips@gmail.com>
2023-06-08 17:39:57 -0700

    python Import Modules

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# File: 5-variable_load.py
+# John Mwadime <johnniekips@gmail.com>
+
+if __name__ == "__main__":
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a
+
+    print(a)
---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#File: add_0.py
+# John Mwadime <johnniekips@gmail.com>
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
