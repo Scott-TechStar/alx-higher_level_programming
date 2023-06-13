c9819fe609bbb9c16e252d75a6d944b49ec7bac6
John Mwadime <johnniekips@gmail.com>
2023-06-13 20:15:10 +0300

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 # 8-multiple_returns.py
-# Brennan D Baraban <375@holbertonschool.com>
-
 
 def multiple_returns(sentence):
     """Returns the length of a string and its first character."""
