8d787d03b009cee6faf85f863ec97ffb7d14a2fb
John Mwadime <johnniekips@gmail.com>
2023-06-13 20:18:20 +0300

    Update 11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 # 11-delete_at.py
-# Brennan D Baraban <375@holbertonschool.com>
-
-
 def delete_at(my_list=[], idx=0):
     """Delete an item at a specific position in a list."""
     if idx >= 0 and idx < len(my_list):
