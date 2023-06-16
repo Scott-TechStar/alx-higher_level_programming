f91b99a87d0b5887ec6928b0960c50361d87b740
John Mwadime <johnniekips@gmail.com>
2023-06-16 03:30:56 +0300

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 # 1-search_replace.py
-# Brennan D Baraban <375@holbertonschool.com
-
+# John Mwadime <johnniekips@gmail.com>
 
 def search_replace(my_list, search, replace):
     """Replace all occurrences of an element by another in a new list."""
