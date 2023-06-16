2fa83cf6a4de4eeae264cc12b9c87bd730889659
John Mwadime <johnniekips@gmail.com>
2023-06-16 03:31:51 +0300

    Update 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 # 100-weight_average.py
-# Brennan D Baraban <375@holbertonschool.com>
-
+# John Mwadime <johnniekips@gmail.com>
 
 def weight_average(my_list=[]):
     """Return the weighted average of all integers in a list of tuples."""
