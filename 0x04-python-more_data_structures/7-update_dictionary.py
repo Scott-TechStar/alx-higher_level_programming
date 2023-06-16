1066a767b228538e8dfa77f16ef29f73cb951eaa
John Mwadime <johnniekips@gmail.com>
2023-06-16 03:36:32 +0300

    Update 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 7-update_dictionary.py
-# Brennan D Baraban <375@holbertonschool.com>
+# John Mwadime <johnniekips@gmail.com>
 
 
 def update_dictionary(a_dictionary, key, value):
