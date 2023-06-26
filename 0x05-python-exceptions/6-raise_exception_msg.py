3a5b96536ccb86bcbd7ea0be6f28287f07db6247
Scott-TechStar <johnniekips@gmail.com>
2023-06-26 02:59:23 -0700

    exceptions

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 6-raise_exception_msg.py
+# John Mwadime
+
+
+def raise_exception_msg(message=""):
+    """Raise a NameError exception with a message."""
+    raise NameError(message)
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # Alx-Higher level programming - Python, Databases & JavaScript
 
 This repository contains programs written for the higher-level programming
