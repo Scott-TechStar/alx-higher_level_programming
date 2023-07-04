f868527053ce152e290d02975d3eda7143ac3e4e
John Mwadime <johnniekips@gmail.com>
2023-07-05 02:20:27 +0300

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -21,55 +21,61 @@ queen must be placed on the chessboard.
 """
 import sys
 
-def solve_nqueens(n):
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-    board = [['.'] * n for _ in range(n)]
-    solutions = []
-
-    def is_safe(row, col):
-        for i in range(row):
-            if board[i][col] == 'Q':
+def nqueens(N):
+    def is_safe(board, row, col):
+        # Check if the current position is attacked by any previous queens
+        # Check horizontally
+        for i in range(col):
+            if board[row][i] == 'Q':
                 return False
-            j = row - i
-            if col - j >= 0 and board[i][col - j] == 'Q':
+        # Check diagonally (upper-left)
+        i, j = row, col
+        while i >= 0 and j >= 0:
+            if board[i][j] == 'Q':
                 return False
-            if col + j < n and board[i][col + j] == 'Q':
+            i -= 1
+            j -= 1
+        # Check diagonally (lower-left)
+        i, j = row, col
+        while i < N and j >= 0:
+            if board[i][j] == 'Q':
                 return False
+            i += 1
+            j -= 1
         return True
 
-    def backtrack(row):
-        if row == n:
-            solutions.append([''.join(row) for row in board])
+    def solve_nqueens(board, col):
+        if col >= N:
+            # Solution found, print the board configuration
+            for row in board:
+                print(' '.join(row))
+            print()
             return
-        for col in range(n):
-            if is_safe(row, col):
+        for row in range(N):
+            if is_safe(board, row, col):
                 board[row][col] = 'Q'
-                backtrack(row + 1)
+                solve_nqueens(board, col + 1)
                 board[row][col] = '.'
-
-    backtrack(0)
-
-    return solutions
-
-if __name__ == '__main__':
+    
+    # Input validation
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
 
     try:
-        n = int(sys.argv[1])
+        N = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
 
-    solutions = solve_nqueens(n)
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
 
-    if not solutions:
-        print("No solutions found.")
-    else:
-        for solution in solutions:
-            print("\n".join(solution))
-            print()
+    # Initialize the board
+    board = [['.' for _ in range(N)] for _ in range(N)]
+    solve_nqueens(board, 0)
+
+# Run the program
+if __name__ == '__main__':
+    nqueens(int(sys.argv[1]))
