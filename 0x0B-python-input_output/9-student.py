18e9fa1d18fe1111e2e02339909eeadef1179024
John Mwadime <johnniekips@gmail.com>
2023-07-11 15:26:52 +0300

    Update 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 # 9-add_item.py
 # John Mwadime
-"""Add all arguments to a Python list and save them to a file."""
-import sys
+"""
+Contains the clas "Student"
+"""
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('8-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+class Student:
+    """Representation of a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes the student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """returns a dictionary representation of a Student instance"""
+        return self.__dict__
