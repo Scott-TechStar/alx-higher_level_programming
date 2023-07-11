bcdd0cec78975db9cc889a506d30809a346b4a36
John Mwadime <johnniekips@gmail.com>
2023-07-11 15:20:48 +0300

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 # 1-number_of_lines.py
 # John Mwadime
-"""Defines a text file line-counting function."""
+"""
+Contains the function "wrtie_file"
+"""
 
 
-def number_of_lines(filename=""):
-    """Return the number of lines in a text file."""
-    lines = 0
-    with open(filename) as f:
-        for line in f:
-            lines += 1
-    return lines
+def write_file(filename="", text=""):
+    """returns the number of chars written to "filename" from "text" """
+    with open(filename, 'w', encoding='utf=8') as f:
+        return f.write(text)
