8ba9ed2c2c7704b25444f98cee13e7c5e423de01
John Mwadime <johnniekips@gmail.com>
2023-08-16 08:21:36 -0700

    Update 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -5,12 +5,6 @@
 -- If the user user_0d_2 already exists, your script should not fail
 -- Create database if not exists
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-
--- Create user if not exists
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-
--- Grant SELECT privilege on hbtn_0d_2 database
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
-
--- Refresh privileges
 FLUSH PRIVILEGES;
