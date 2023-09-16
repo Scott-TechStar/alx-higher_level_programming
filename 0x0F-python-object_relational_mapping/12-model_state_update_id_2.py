29831159140ff9041e47961c5b6469f91d101877
John Mwadime <johnniekips@gmail.com>
2023-09-16 07:43:32 -0700

    Update

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-# Changes the name of the State object with id = 2 to
-# New Mexico in the database hbtn_0e_6_usa.
-# Usage: ./12-model_state_update_id_2.py <mysql username> /
-#                                        <mysql password> /
-#                                        <database name>
-import sys
+"""
+Script that changes the name of a State object to the database
+Using module SQLAlchemy
+"""
+
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from sys import argv
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    # create an engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    # create a configured "Session" class
     Session = sessionmaker(bind=engine)
+    # create a Session
     session = Session()
-
-    state = session.query(State).filter_by(id=2).first()
-    state.name = "New Mexico"
+    Base.metadata.create_all(engine)
+    state_update = session.query(State).filter_by(id='2').first()
+    state_update.name = "New Mexico"
+    # commit and close session
     session.commit()
+    session.close()
