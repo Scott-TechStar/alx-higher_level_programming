5db9c9939cbc9a32cc9fc62f9188205eb760a4f8
John Mwadime <johnniekips@gmail.com>
2023-09-16 07:43:15 -0700

    Update

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
-# Adds the State object "Louisiana" to the database hbtn_0e_6_usa.
-# Usage: ./11-model_state_insert.py <mysql username> /
-#                                   <mysql password> /
-#                                   <database name>
-import sys
+"""
+Script that adds the State object “Louisiana” to the database
+Using module SQLAlchemy
+"""
+
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from sys import argv
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    # create an engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    # create a configured "Session" class
     Session = sessionmaker(bind=engine)
+    # create a Session
     session = Session()
+    Base.metadata.create_all(engine)
 
-    louisiana = State(name="Louisiana")
-    session.add(louisiana)
+    add_state = State(name="Louisiana")
+    session.add(add_state)
+    # commit and close session
     session.commit()
-    print(louisiana.id)
+    print(add_state.id)
+    session.close()
