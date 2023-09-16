2fb37580793a5ffb0c5fc5f70bd62de66fab3c7a
John Mwadime <johnniekips@gmail.com>
2023-09-16 07:47:22 -0700

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
 """
-Script that takes in an argument and displays all values
-in the states table of hbtn_0e_0_usa where name matches the argument
-but safe from MySQL injections!
+Script that takes in the name of a state as an argument and lists
+all cities of that state, using the database
 """
 import MySQLdb
 from sys import argv
 
 # The code should not be executed when imported
 if __name__ == '__main__':
-
     # make a connection to the database
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
 
-    # It gives us the ability to have multiple seperate working environments
-    # through the same connection to the database.
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name = %s", [argv[4]])
+    cur.execute("SELECT cities.id, cities.name FROM cities\
+                INNER JOIN states ON cities.state_id = states.id\
+                WHERE states.name = %s", [argv[4]])
 
     rows = cur.fetchall()
+    j = []
     for i in rows:
-        print(i)
+        j.append(i[1])
+    print(", ".join(j))
+
     # Clean up process
     cur.close()
     db.close()
