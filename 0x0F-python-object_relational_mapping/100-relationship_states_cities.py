96342cc497e7b8a1330ed4069676256a8b3ec9fc
John Mwadime <johnniekips@gmail.com>
2023-09-17 03:01:23 -0700

    Update 100-relationship_states_cities.py

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -3,24 +3,32 @@
 with the City “San Francisco”
 to the database hbtn_0e_100_usa"""
 
-if __name__ == "__main__":
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
 
-    import sys
-    from relationship_state import Base, State
-    from relationship_city import City
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    from sqlalchemy.schema import Table
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+def create_state_city(username, password, db_name):
+   engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    session = Session(engine)
-    new_city = City(name='San Francisco')
-    new = State(name='California')
-    new.cities.append(new_city)
-    session.add_all([new, new_city])
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name="California")
+    new_city = City(name="San Francisco", state=new_state)
+
+    session.add(new_state)
     session.commit()
+
     session.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python 100-relationship_states_cities.py <username> <password> <db_name>")
+    else:
+        username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+        create_state_city(username, password, db_name)
