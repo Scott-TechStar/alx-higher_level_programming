15e063c214bef976ec08275f07017100794a7ac4
John Mwadime <johnniekips@gmail.com>
2023-09-17 03:15:36 -0700

    Update 102-relationship_cities_states_list.py

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+"""List all states"""
+from sys import argv
 from relationship_state import Base, State
 from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
-def list_cities(username, password, db_name):
-    engine = create_engine(f"mysql+mysqlconnector://{username}:{password}@localhost:3306/{db_name}")
-    Base.metadata.create_all(engine)
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2],
+                argv[3]), pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    cities = session.query(City).order_by(City.id).all()
-
-    for city in cities:
-        print(f"{city.name} ({city.state.name})")
-
-    session.close()
-
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python list_cities.py <username> <password> <db_name>")
-    else:
-        username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
-        list_cities(username, password, db_name)
+    for city, state in session.query(City, State)\
+                              .join(State, State.id == City.state_id)\
+                              .order_by(City.id):
+        print("{}: {} -> {}".format(city.id, city.name, state.name))
