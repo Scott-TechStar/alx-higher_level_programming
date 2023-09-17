bac738f3f81ad9faa1f1597532d7451bfbfe8b6d
John Mwadime <johnniekips@gmail.com>
2023-09-17 03:14:09 -0700

    Update 100-relationship_states_cities.py

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,34 +1,23 @@
 #!/usr/bin/python3
-"""adds the State object “California”
-with the City “San Francisco”
-to the database hbtn_0e_100_usa"""
-
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+"""List all states"""
+from sys import argv
 from relationship_state import Base, State
 from relationship_city import City
-
-def create_state_city(username, password, db_name):
-   engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    new_state = State(name="California")
-    new_city = City(name="San Francisco", state=new_state)
-
-    session.add(new_state)
-    session.commit()
-
-    session.close()
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python 100-relationship_states_cities.py <username> <password> <db_name>")
-    else:
-        username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
-        create_state_city(username, password, db_name)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(argv[1], argv[2],
+                argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_state = State(name="California")
+    session.add(new_state)
+    session.commit()
+    new_city = City(name="San Francisco", state_id=new_state.id)
+    session.add(new_city)
+    session.commit()
+    session.close()
