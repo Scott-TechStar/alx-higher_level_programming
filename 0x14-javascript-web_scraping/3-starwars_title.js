9fe72df394ce9b1faeb229e4e1bf6bc0062e5295
Scott-TechStar <johnniekips@gmail.com>
2023-10-24 09:40:56 -0700

    title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
-const request = require('request');
-const { argv } = require('process');
 
-const BaseUrl = 'https://swapi-api.alx-tools.com/api/';
-request(BaseUrl + '/films/' + argv[2], (error, response, body) => {
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(url, (error, response, body) => {
   if (error) {
-    console.error(error);
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    console.log(data.title);
   }
-  console.log(JSON.parse(body).title);
 });
